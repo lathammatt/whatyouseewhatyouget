2eac6c2809b02feb905fa059ad5960a85681d205
Matt Latham <latham75@gmail.com>
2016-06-11 15:59:21 -0500

    output html first run

--- mainjs.js
+++ mainjs.js
@@ -4,7 +4,7 @@ var output = document.getElementById("main");
 var counter = 0;
 
 for (; counter <5; counter++){
-	output.innerHTML += `<div class="personbox" id="person -${counter}><ul><li><h2>Occupation:</h2></div>`;
+	output.innerHTML += `<div class="personbox" id="person -${counter}><ul><li><h2>Occupation:</h2>${object.famous[i].title}</li><li><h2>Name:</h2>${object.famous[i].name}</li><li><h2>Bio:</h2>${object.famous[i].bio}</li><li><h2>Years Lived:</h2>${object.famous[i].lifespan.birth}+"-"+${object.famous[i].lifespan.death}</li></div>`;
 }
 
 var personnel = document.getElementsByClassName("personbox");

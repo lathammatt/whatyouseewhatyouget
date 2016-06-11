ebc08b2b31b810bdbbbdd5c640bf567feed11b70
Matt Latham <latham75@gmail.com>
2016-06-11 15:54:08 -0500

    creating personnel doms

--- mainjs.js
+++ mainjs.js
@@ -1,4 +1,16 @@
 var userChanges = document.getElementById("input");
 var output = document.getElementById("main");
 
+var counter = 0;
 
+for (; counter <5; counter++){
+	output.innerHTML += `<div class="personbox" id="person -${counter}><ul><li><h2>Occupation:</h2></div>`;
+}
+
+var personnel = document.getElementsByClassName("personbox");
+
+for (var i = 0; i > personnel.length; i++){
+	personnel[i].addEventListener("click", function(event){
+		// click logic here
+	});
+};

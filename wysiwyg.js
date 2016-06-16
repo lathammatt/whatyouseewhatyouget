fb4f9b388fdc8c48a8a73b218618d9fb78dd4a53
Matt Latham <latham75@gmail.com>
2016-06-16 14:49:36 -0500

    remove string interpolation, fixed pictures

--- wysiwyg.js
+++ wysiwyg.js
@@ -13,7 +13,7 @@ var famous = [
   title: "General",
   name: "Ulysses Grant",
   bio: "As Commanding General of the United States Army (1864–69), Grant worked closely with President Abraham Lincoln to lead the Union Army to victory over the Confederacy in the American Civil War.",
-  image: "https://en.wikipedia.org/wiki/Ulysses_S._Grant#/media/File:Ulysses_Grant_1870-1880.jpg",
+  image: "https://upload.wikimedia.org/wikipedia/commons/d/d7/Ulysses_Grant_1870-1880.jpg",
   lifespan: {
     birth: 1822,
     death: 1885
@@ -23,7 +23,7 @@ var famous = [
   title: "King",
   name: "Henry V",
   bio: "King of England from 1413 until his death at the age of 36 in 1422. He was the second English monarch who came from the House of Lancaster.",
-  image: "https://en.wikipedia.org/wiki/Henry_V_of_England#/media/File:King_Henry_V_from_NPG.jpg",
+  image: "https://upload.wikimedia.org/wikipedia/commons/1/18/King_Henry_V_from_NPG.jpg",
   lifespan: {
     birth: 1386,
     death: 1422
@@ -33,7 +33,7 @@ var famous = [
   title: "Ninja",
   name: "Hattori Hanzō",
   bio: "A famous samurai of the Sengoku era, credited with saving the life of Tokugawa Ieyasu and then helping him to become the ruler of united Japan.",
-  image: "https://en.wikipedia.org/wiki/Hattori_Hanz%C5%8D#/media/File:Hanzo.jpg",
+  image: "https://upload.wikimedia.org/wikipedia/commons/c/ca/Hanzo.jpg",
   lifespan: {
     birth: 1542,
     death: 1596
@@ -58,8 +58,55 @@ var output = document.getElementById("main");
 var counter = 0;
 
 for (; counter < famous.length; counter++){
-	var people = `<div class="personbox" id="person-${counter}"><ul><li><h2>Occupation:</h2>${famous[counter].title}</li><li><h2>Name:</h2>${famous[counter].name}</li><li><h2>Bio:</h2><div id="bio-${counter}">${famous[counter].bio}</div></li><li><h2>Years Lived:</h2>${famous[counter].lifespan.birth}-${famous[counter].lifespan.death}</li></div>`;
-	output.innerHTML += people;
+	var mainPeeps = document.createElement("div");
+	mainPeeps.className = "personbox";
+	mainPeeps.id = "person-" + `${counter}`;
+	document.getElementById("main").appendChild(mainPeeps);
+	var listStart = document.createElement("ul");
+	listStart.className = "list";
+	listStart.id = "list-" + `${counter}`;
+	mainPeeps.appendChild(listStart);
+	var mouse = document.createAttribute("onclick");
+	mouse.value = "clickIt()";
+	listStart.setAttributeNode(mouse);
+	var picture = document.createElement("img");
+	listStart.appendChild(picture);
+	picture.className = "property";
+	picture.id = "pic-" + `${counter}`;
+	picture.src = famous[counter].image;
+	var job = document.createElement("li");
+	job.className = "key";
+	listStart.appendChild(job);
+	job.appendChild(document.createTextNode("Occupation:"));
+	var jobName = document.createElement("li");
+	listStart.appendChild(jobName);
+	jobName.className = "property";
+	jobName.appendChild(document.createTextNode(famous[counter].title));
+	var bob = document.createElement("li");
+	bob.className = "key";
+	listStart.appendChild(bob);
+	bob.appendChild(document.createTextNode("Name:"));
+	var character = document.createElement("li");
+	listStart.appendChild(character);
+	character.className = "property";
+	character.appendChild(document.createTextNode(famous[counter].name));
+	var bio = document.createElement("li");
+	listStart.appendChild(bio);
+	bio.className = "key";
+	bio.appendChild(document.createTextNode("Bio:"));
+	var bioedit = document.createElement("li");
+	listStart.appendChild(bioedit);
+	bioedit.className = "property";
+	bioedit.id = "bio-" + `${counter}`;
+	bioedit.appendChild(document.createTextNode(famous[counter].bio));
+	var years = document.createElement("li");
+	listStart.appendChild(years);
+	years.className = "key";
+	years.appendChild(document.createTextNode("Years Lived:"));
+	var dates = document.createElement("li");
+	listStart.appendChild(dates);
+	dates.className = "property";
+	dates.appendChild(document.createTextNode(famous[counter].lifespan.birth + "-" + famous[counter].lifespan.death));
 };
 
 var personnel = document.getElementsByClassName("personbox");
@@ -68,8 +115,8 @@ var personnel = document.getElementsByClassName("personbox");
 
 for (var i = 0; i > personnel.length; i++){
 	personnel[i].addEventListener("click", function borderIt(event){
-		personnel.classList.toggle("caged");
 		console.log("event", event);
+		personnel.classList.toggle("caged");
 	});
 };
 
@@ -81,6 +128,18 @@ userChanges.addEventListener("keyup", function processKeys(event){
 		console.log("return", event);
 		}
 });
+
+// var dotted = document.getElementsByClassName("list");
+
+// for (var i = 0; i < dotted.length; i++) {
+// 	console.log("event", event);
+//   	dotted[i].addEventListener("click", function (event) {
+//   	.classList.add("dotted");
+// })};
+function clickIt(event){
+	listStart.classList.toggle("caged");
+}
+
 
 
 // how to register clicked div
